f4a0e0e3ca6c86395919e7da7dedac1ef7cf19b1
J.J. Idt & Kyle Boe <student@epicodus.com>
2014-07-29 09:56:43 -0700

    add player class

--- spec/go_fish_spec.rb
+++ spec/go_fish_spec.rb
@@ -7,13 +7,20 @@ describe 'Game' do
     test_game = Game.new
     test_game.should be_an_instance_of Game
   end
+
   it 'creates a game with a deck' do
     test_game = Game.new
     test_game.deck.is_a?(Array).should eq true
   end
+
   it 'randomly orders 52 cards in the deck' do
     test_game = Game.new
     test_game.deck.length.should eq 52
+  end
+
+  it 'creates two players on initialize' do
+    test_game = Game.new
+    test_game.players[0].should be_an_instance_of Player
   end
 end
 
@@ -22,4 +29,5 @@ describe 'Player' do
     test_player = Player.new(1)
     test_player.should be_an_instance_of Player
   end
+
 end
