2e35d0e8ec3ac03e043597f944672d599e4eeb14
J.J. Idt & Kyle Boe <student@epicodus.com>
2014-07-29 09:48:37 -0700

    make the deck

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,8 @@
 class Game
   def initialize
-    @deck = []
+    cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    suits = ["H", "D", "S", "C"]
+    @deck = cards.product(suits).shuffle
   end
   def deck
     @deck
